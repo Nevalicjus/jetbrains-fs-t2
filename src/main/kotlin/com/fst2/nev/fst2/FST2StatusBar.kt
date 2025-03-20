94f15bdce8d33159535ada4e5eb540727def3c06
Maciej Bromirski <maciej.bromirski@gmail.com>
2025-03-20 14:51:41 +0100

    some refactoring

--- src/main/kotlin/com/fst2/nev/fst2/FST2StatusBar.kt
+++ src/main/kotlin/com/fst2/nev/fst2/FST2StatusBar.kt
@@ -9,14 +9,13 @@ import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.StatusBarWidget.WidgetPresentation
 import com.intellij.openapi.wm.StatusBar
+import java.awt.event.MouseEvent
+import com.intellij.util.Consumer
 
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.*
-import com.intellij.psi.util.elementType
-import java.awt.event.MouseEvent
-import com.intellij.util.Consumer
 import com.jetbrains.python.psi.*
 import com.jetbrains.python.psi.impl.*
 
@@ -24,18 +23,21 @@ class FST2StatusBar : StatusBarWidget, StatusBarWidget.TextPresentation {
     private var statusBar: StatusBar? = null
     private var currentType: String? = null
     private var currentName: String? = null
+    private var debug: String? = null
 
-    // shamelessly adapted from
+    // shamelessly adapted from JetBrains' PositionPanel Plugin
     // https://github.com/JetBrains/intellij-community/blob/3604aef6c833623213170816279b267bf2f5f698/platform/platform-impl/src/com/intellij/openapi/wm/impl/status/PositionPanel.kt
     init {
         val disposable = Disposer.newDisposable()
         val multicaster = EditorFactory.getInstance().eventMulticaster
         multicaster.addCaretListener(object: CaretListener {
             override fun caretPositionChanged(e: CaretEvent) {
-                // when multiple carets exist in editor, we don't show information about caret positions
                 val editor = e.editor
+                // with multiple carets we don't check types
                 if (editor.caretModel.caretCount == 1) {
                     updateType(e, editor)
+                } else {
+                    setCurrents(null, null)
                 }
             }
 
@@ -54,44 +56,48 @@ class FST2StatusBar : StatusBarWidget, StatusBarWidget.TextPresentation {
         }, disposable)
     }
 
-    override fun ID(): String {
-        return "FST2StatusBar"
-    }
-
-    override fun getText(): String {
-        return this.currentType ?: ":("
-    }
-
     fun updateType(event: CaretEvent, editor: Editor) {
-        val caret = event.caret ?: return // if caret isn't null ...
-        val project = editor.project ?: return // ... and project isn't null ...
+        val caret = event.caret ?: return
+        val project = editor.project ?: return
         val psiFile: PsiFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.document) ?: return
-        // ... then from the current document get the PsiFile
         val offset = caret.offset
         val elementAtCaret: PsiElement = psiFile.findElementAt(offset) ?: return
-        // ... and if the PsiElement at caret isn't null ...
+        // get the 'Expression' parent of that PsiElement
         val exprEl = PsiTreeUtil.getParentOfType(elementAtCaret, PsiElement::class.java) ?: return
-        // ... get the 'Expression' parent of that PsiElement
+        this.debug = "${exprEl.node.elementType}, ${exprEl.text}"
 
         var castedPt = (exprEl as? PyTargetExpression)
-        // if it doesn't cast to PyTargetExpression ...
+        // if it doesn't cast to PyTargetExpression (at the definition),
+        // try as a PyReferenceExpression (at an use)
         if (castedPt == null) {
-            castedPt = this.PyRef2PyTar(exprEl as? PyReferenceExpression)
-        } // ... recast
-        // and if still couldn't cast ...
+            castedPt = this.pyReference2PyTarget(exprEl as? PyReferenceExpression)
+        }
+        // and if still couldn't cast, it wasn't a variable, so we update to null
         if (castedPt == null) {
-            // (means this isn't a variable)
-            this.currentType = null
-            this.currentName = null
-            this.statusBar?.updateWidget(ID())
+            this.setCurrents(null, null)
             return
-        } // ... unset values and return
-        // ... and update values
-        this.updateFromPyTar(castedPt)
+        }
+        val rhs: PsiElement? = this.pyTarget2RHS(castedPt)
+        this.setCurrents(this.nodeToType(rhs), this.nodeToName(rhs))
+    }
+
+    private fun nodeToName(el: PsiElement?): String {
+        if (el == null) return "?"
+        val castedParent = (el.parent as? PyAssignmentStatement) ?: return "?"
+        return castedParent.firstChild.text
+    }
+    private fun nodeToType(el: PsiElement?): String {
+        if (el == null) return "?"
+        return this.py2Name(el.node.elementType.toString(), el)
+    }
+
+    private fun setCurrents(type: String?, name: String?) {
+        this.currentType = type
+        this.currentName = name
         this.statusBar?.updateWidget(ID())
     }
 
-    private fun Py2Name(pn: String, el: PsiElement): String {
+    private fun py2Name(pn: String, el: PsiElement): String {
         return when(pn) {
             "Py:STRING_LITERAL_EXPRESSION" -> "str"
             "Py:INTEGER_LITERAL_EXPRESSION" -> "int"
@@ -99,17 +105,22 @@ class FST2StatusBar : StatusBarWidget, StatusBarWidget.TextPresentation {
             "Py:FLOAT_LITERAL_EXPRESSION" -> "float"
             "Py:DICT_LITERAL_EXPRESSION" -> "dict"
             "Py:SET_LITERAL_EXPRESSION" -> "set"
-            "Py:BINARY_EXPRESSION" -> el.children.joinToString(separator = ", ") { this.Py2NameRefHelper(it) }
+            "Py:BINARY_EXPRESSION" -> el.children.joinToString(
+                separator = ", ", transform = { this.py2NameRefHelper(it) }
+            ) 
+            // this is sometimes not perfect; an expression "7+7" will be resolved to "int, int",
+            // and when used in a function call later, "f(int, int)",
+            // which will be understood as a function that takes two ints as parameters
             "Py:PARENTHESIZED_EXPRESSION" -> {
                 val castedMiddle = (el.firstChild.nextSibling as? PyTupleExpression)
                 if (castedMiddle != null) { "tuple" } else { "tuple?" }
             }
             "Py:REFERENCE_EXPRESSION" -> {
-                val recastPr = this.PyRef2PyTar(el as? PyReferenceExpression) ?: "?"
-                this.Py2Name(recastPr.toString(), el)
-            } // for nesteds
+                val recastPr = this.pyReference2PyTarget(el as? PyReferenceExpression) ?: "?"
+                this.py2Name(recastPr.toString(), el)
+            }
             "Py:CALL_EXPRESSION" -> "f(" + el.lastChild.children.joinToString(
-                separator = ",", transform = { this.Py2NameRefHelper(it) }
+                separator = ", ", transform = { this.py2NameRefHelper(it) }
             ) + ")"
             "ERROR_ELEMENT" -> ":("
             else -> {
@@ -118,65 +129,42 @@ class FST2StatusBar : StatusBarWidget, StatusBarWidget.TextPresentation {
         }
     }
 
-    // Unwinds a PyReferenceExpression into it's PyTargetExpression
-    private fun PyRef2PyTar(el: PyReferenceExpression?): PyTargetExpression? {
+    private fun pyReference2PyTarget(el: PyReferenceExpression?): PyTargetExpression? {
         if (el == null) return null
         val prRef = (el.reference.resolve() as? PyTargetExpression) ?: return null
         return prRef
     }
 
-    // Unwinds a suspected PyReferenceExpression into rhs of a PyAssignmentStatement
-    private fun PyRefish2PyRef(el: PsiElement): PsiElement? {
-        val prRef = (el.reference?.resolve() as? PyTargetExpressionImpl) ?: return null
+    private fun pyReference2RHS(el: PyReferenceExpression?): PsiElement? {
+        val prRef = (el?.reference?.resolve() as? PyTargetExpressionImpl) ?: return null
         val castedParent = (prRef.parent as? PyAssignmentStatement) ?: return null
-        val rhs = castedParent.lastChild
-        return rhs
+        return castedParent.lastChild // rhs
+    }
+
+    private fun pyTarget2RHS(el: PyTargetExpression?): PsiElement? {
+        val castedParent = (el?.parent as? PyAssignmentStatement) ?: return null
+        return castedParent.lastChild // rhs
     }
 
     // In a PyBinaryExpression and PyCallExpression we want the same thing:
     // Turn PyReferenceExpression into it's representation - but even though
     // they report "Py:REFERENCE_EXPRESSION" they're sometimes a PyTargetExpressionImpl
     // so this function unwinds them
-    private fun Py2NameRefHelper(el: PsiElement?): String {
-        if (el == null) return ""
-        if (el.node.elementType.toString() == "Py:REFERENCE_EXPRESSION") {
-            val rhs = this.PyRefish2PyRef(el)
-            if (rhs != null) {
-                return this.Py2Name(rhs.elementType.toString(), rhs)
-            } else {
-                return "?"
-            }
+    private fun py2NameRefHelper(el: PsiElement?): String {
+        if (el == null) {
+            return ""
+        } else if (el.node.elementType.toString() == "Py:REFERENCE_EXPRESSION") {
+            return this.nodeToType(this.pyReference2RHS((el as? PyReferenceExpression)))
         } else {
-            return this.Py2Name(el.node.elementType.toString(), el)
+            return this.py2Name(el.node.elementType.toString(), el)
         }
     }
 
-    private fun updateFromPyTar(el: PyTargetExpression) {
-        val castedParent = (el.parent as? PyAssignmentStatement) ?: return
-        // ... get our PyAssignmentStatement parent ...
-        val rhs = castedParent.lastChild ?: return
-        // ... and their assigned value ...
-        this.currentType = this.Py2Name(rhs.node.elementType.toString(), rhs)
-        // ... and set our type to its String name converted to a familiar Python type name ...
-        this.currentName = castedParent.firstChild?.text
-        // ... and set our name to its String name ...
-    }
-
-    override fun getClickConsumer(): Consumer<MouseEvent> = Consumer {
-        println("currentType = ${this.currentType}!")
-        this.statusBar?.updateWidget(ID())
-    }
-
-    override fun getPresentation(): WidgetPresentation {
-        return this
-    }
-    override fun getAlignment(): Float {
-        return 0.0f // left
-    }
-    override fun install(statusBar: StatusBar) {
-        this.statusBar = statusBar
-    }
-    override fun getTooltipText(): String {
-        return "Current variable's name is: ${this.currentName ?: "unknown :("}"
-    }
+    override fun ID(): String { return "FST2StatusBar" }
+    override fun getText(): String { return this.currentType ?: ":(" }
+    override fun getTooltipText(): String { return this.currentName ?: ":(" }
+    override fun getClickConsumer(): Consumer<MouseEvent> = Consumer { println("Debug: ${this.debug}") }
+    override fun getPresentation(): WidgetPresentation { return this }
+    override fun getAlignment(): Float { return 0.0f }
+    override fun install(statusBar: StatusBar) { this.statusBar = statusBar }
 }
--- src/main/kotlin/com/fst2/nev/fst2/FST2WidgetFactory.kt
+++ src/main/kotlin/com/fst2/nev/fst2/FST2WidgetFactory.kt
@@ -7,14 +7,9 @@ import com.intellij.openapi.wm.StatusBarWidgetFactory
 
 class FST2WidgetFactory : StatusBarWidgetFactory {
     override fun getId(): String { return "FST2StatusBar" }
-
     override fun getDisplayName(): String { return "FST2 Status Bar Widget" }
-
     override fun isAvailable(project: Project): Boolean { return true }
-
     override fun createWidget(project: Project): StatusBarWidget { return FST2StatusBar() }
-
     override fun disposeWidget(widget: StatusBarWidget) {}
-
     override fun canBeEnabledOn(statusBar: StatusBar): Boolean { return true }
 }
